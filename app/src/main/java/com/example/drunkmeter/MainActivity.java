b97ddaa700afcb8e744a328fc120b9c9bcb6d9b4
Keren Romero <keren@Kerens-MacBook-Air.local>
2020-05-29 14:34:57 -0700

    Initial commit

---
+++ app/src/main/java/com/example/drunkmeter/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.drunkmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button beginTest;
+    private Button pastRes;
+    private Button settings;
+    private Button info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        beginTest = (Button)findViewById(R.id.beginTest);
+        pastRes = (Button)findViewById(R.id.pastRes);
+        settings = (Button)findViewById(R.id.settings);
+        info = (Button)findViewById(R.id.info);
+
+        beginTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Begin_Test.class);
+                startActivity(intent);
+            }
+        });
+
+        pastRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Past_Res.class);
+                startActivity(intent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Info.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
